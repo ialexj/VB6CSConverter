a38a9046dd7e6a7f66a7bbde965bc57683f053d8
Alex Jerebtsov <aj@inforbase.pt>
2026-07-06 14:31:41 +0100

    Tweaks do diagnostics report

--- src/VB6Converter/DiagnosticsReport.cs
+++ src/VB6Converter/DiagnosticsReport.cs
@@ -9,30 +9,19 @@ public static class DiagnosticsReport
 {
     public static void Write(TextWriter writer, IReadOnlyCollection<Diagnostic> diagnostics)
     {
+        writer.WriteLine($"Diagnostics Report - {DateTime.Now}");
+        writer.WriteLine("Global");
+        writer.WriteLine("=======================================================");
+
         diagnostics = diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error).ToArray();
-
-        writer.WriteLine();
-        writer.WriteLine("Global Diagnostics:");
-
         WriteStatistics(writer, diagnostics, false);
 
         writer.WriteLine();
-        writer.WriteLine("=======================================================");
-        writer.WriteLine("Files:");
 
         foreach (var file in diagnostics.Where(d => d.Location?.SourceTree != null).GroupBy(d => d.Location.SourceTree.FilePath).OrderByDescending(f => f.Count())) {
-            writer.WriteLine($"--- {Path.GetFileNameWithoutExtension(file.Key)} ---");
+            writer.WriteLine($"{Path.GetFileNameWithoutExtension(file.Key)}");
+            writer.WriteLine("=======================================================");
             WriteStatistics(writer, file, true);
-
-            writer.WriteLine();
-
-            var messages = file.GroupBy(gg => gg.GetMessage())
-                .Select(g => (message: g.Key, count: g.Count()))
-                .OrderByDescending(g => g.count);
-
-            foreach (var id in messages) {
-                writer.WriteLine($"      {id.count,-6} - {id.message}");
-            }
         }
     }
 
@@ -46,7 +35,7 @@ public static class DiagnosticsReport
                 .OrderByDescending(g => g.count);
 
             foreach (var id in ids) {
-                writer.WriteLine($"{id.count,-6} - {id.id.Id} {id.id.MessageFormat}");
+                writer.WriteLine($"[{id.count,-6} ] {id.id.Id} {id.id.MessageFormat}");
 
                 if (detail) {
                     foreach (var diag in id.g) {
